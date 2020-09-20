26cb206e8ade072425cdaa3d86a23b938b0c5a86
Елена Белых <Eleyurgab@gmail.com>
2020-09-20 14:53:13 +0300

    Program fo sum elements in array from firstIndex to lastIndex

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp8
+{
+
+    class Program
+    {
+        public static int[] sumNumbersList = new List<int>();
+        public static int[] outputList = new List<int>();
+        public static string sumNumbers = "";
+        static int Main(string[] args)
+        {
+            ReadFromConsole();
+            return 0;
+
+        }
+
+        public static void ReadFromConsole()
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            string line;
+            int firstIndex;
+            int LastIndex;
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                int inputNumber = int.Parse(Console.ReadLine());
+                addNumberToList(inputNumber);
+            }
+
+            int numbersDistanceCount = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbersDistanceCount; i++)
+            {
+                line = Console.ReadLine();
+                int indexOfSpace = line.IndexOf(' ');
+
+                firstIndex = int.Parse(line.Substring(0, indexOfSpace));
+                LastIndex  = int.Parse(line.Substring(indexOfSpace + 1));
+                addSumNumbersToList(firstIndex, LastIndex);
+            }
+            Console.WriteLine(sumNumbers.Substring(0, sumNumbers.Length -1));
+        }
+
+        public static void addNumberToList(int inputNumber)
+        {
+            int listLength = sumNumbersList.Count;
+            int sumOfElements = inputNumber;
+            sumNumbersList[0] = 0;
+            if (listLength >= 1)
+                sumOfElements += sumNumbersList[listLength - 1];
+
+            sumNumbersList.Add(sumOfElements);
+        }
+
+        public static void addSumNumbersToList(int firstIndex, int lastIndex)
+        {
+            int result = 0;
+
+            if (firstIndex == 1)
+                result = sumNumbersList[lastIndex - 1];
+
+            if (result.Equals(0))
+                result = sumNumbersList[lastIndex - 1] - sumNumbersList[firstIndex - 2];
+            sumNumbers += result + "\n";
+        }
+    }
+}
---
+++ ConsoleApp8/test.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ConsoleApp8
+{
+    class Test
+    {
+
+        public static List<int> GenerateNumbers(int elementsCount)
+        {
+            Random random = new System.Random();
+            List<int> resultList = new List<int>();
+
+            if (elementsCount > 0)
+            {
+                for (int i = 0; i < elementsCount; i++)
+                {
+                    int num = random.Next(-10, 10);
+                    resultList.Add(num);
+                }
+            }
+
+            return resultList;
+        }
+        public static int SumOfNumbers(int firstIndex, int lastIndex, List<int> list)
+        {
+            int sumOfNumbers = 0;
+            string s = "";
+            for (int i = firstIndex; i <= lastIndex; i++)
+            {
+                sumOfNumbers += list[i-1];
+            }
+
+            Console.WriteLine(s);
+            return sumOfNumbers;
+        }
+        //public static void Test1(List<int> list)
+        //{
+        //    int sumOfNumbers = 0;
+        //    foreach (var element in list)
+        //        Program.addNumberToList(element);
+
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        Random random = new System.Random();
+        //        int firstIndex_param = random.Next(1, 5);
+        //        int lastIndex_param = random.Next(5, list.Count);
+        //        sumOfNumbers = SumOfNumbers(firstIndex_param, lastIndex_param, list);
+        //        Console.WriteLine("A[{0}][{1}] = {2}    {3}", firstIndex_param, lastIndex_param, sumOfNumbers, Program.addSumNumbersToList_n(firstIndex_param, lastIndex_param));
+        //    }
+        //        Console.WriteLine("A[{0}][{1}] = {2}    {3}", 2, 2, SumOfNumbers(2, 2, list), Program.addSumNumbersToList_n(2, 2));
+        //        Console.WriteLine("A[{0}][{1}] = {2}    {3}", 3, 3, SumOfNumbers(3, 3, list), Program.addSumNumbersToList_n(3, 3));
+        //        Console.WriteLine("A[{0}][{1}] = {2}    {3}", 4, 5, SumOfNumbers(4, 5, list), Program.addSumNumbersToList_n(4, 5));
+        //}
+
+    }
+}
